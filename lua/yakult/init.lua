ff2f381cbed15b4c33f9385546e56620c71fe84e
ilias-vas <yakult1032@gmail.com>
2024-04-08 22:09:40 +1000

    Initial Commit

---
+++ lua/yakult/init.lua
@@ -0,0 +1,118 @@
+require("yakult.remap")
+require("yakult.set")
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+-- General Plugins
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-treesitter/nvim-treesitter', {["do"] = ':TSUpdate'})
+Plug('mbbill/undotree')
+Plug('kdheepak/lazygit.nvim')
+Plug('m4xshen/autoclose.nvim')
+Plug('akinsho/toggleterm.nvim', {['tag'] = '*'})
+Plug('comfysage/evergarden')
+Plug('nvim-lualine/lualine.nvim')
+
+-- LSP Support
+Plug('neovim/nvim-lspconfig')
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+Plug('VonHeikemen/lsp-zero.nvim', {["branch"] = 'v3.x'})
+
+-- Autocompletion
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('saadparwaiz1/cmp_luasnip')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-nvim-lua')
+
+-- Snippets
+Plug('L3MON4D3/LuaSnip')
+Plug('rafamadriz/friendly-snippets')
+
+vim.call('plug#end')
+
+-- THEME SETTINGS
+require 'evergarden'.setup {
+    transparent_background = true
+}
+vim.cmd[[colorscheme evergarden]]
+
+-- LSP SETTINGS
+local lsp_zero = require('lsp-zero')
+
+-- LSP Setup
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here 
+  -- with the ones you want to install
+  ensure_installed = {'tsserver', 'rust_analyzer', 'jdtls', 'lua_ls', 'jedi_language_server', 'bashls', 'arduino_language_server'},
+  handlers = {
+    lsp_zero.default_setup,
+
+    ["lua_ls"] = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup {
+            settings = {
+                Lua = {
+                    diagnositcs = {
+                        globals = { "vim", "capabiliti1C1C1Ces" }
+                    }
+                }
+            }
+        }
+    end,
+  }
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+
+cmp.setup({
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    window = {
+        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
+
+
+
+
+
+
