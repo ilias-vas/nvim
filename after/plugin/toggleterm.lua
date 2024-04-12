856da97b45f15d8872407eb1f05e8c794415cd0c
ilias-vas <yakult1032@gmail.com>
2024-04-12 15:10:21 +1000

    add lazygit keybind to toggleterm

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -29,3 +29,12 @@ toggleterm.setup {
 local keymap = vim.keymap.set
 local s_opts = { silent = true }
 keymap("t", "<esc>", [[<C-\><C-n>]], s_opts)
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
